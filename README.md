50ad400563a754beada2351921ddee3d9a611125
Shawn Liu <xliu@siocsxliu.local>
2017-06-07 13:39:52 -0400

    update read me
    10.10.5.130 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBF4Nmj1R8wFIp/3wZpciUCKGnH5rzs50N01YvmcnA7eKmKAW7TI0HeFhKyZFPnEKF1kqo00KahqrST8P+mMOr78=

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Algorithm-Refresh-Javascript-
+
+This is a Node.js project, the code cannot be run on browser.
+
+To run the project: type command "node main.js" in the terminal
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function testBinaryTreePath() {
     let root = BTP.createNode(3);
     root.left = BTP.createNode(1);
     root.right = BTP.createNode(5);
-    console.log(BTP.binaryTreePaths(root));
+    console.log('binary Tree Path result: ', BTP.binaryTreePaths(root));
 }
 
 testBinaryTreePath();
