6efb6efea8a09d5240e3f7e287be255d8b817446
Shawn Liu <xliu@siocsxliu.local>
2017-06-07 14:51:42 -0400

    Largest Value in Tree Row

---
+++ largestValueInTreeRow.js
@@ -0,0 +1,32 @@
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+};
+
+module.exports = {
+    largestValues(root) {
+        let result = [];
+        let curRow = [];
+        let nextRow = [];
+        let curLargest = -Number.MAX_VALUE;
+        if (!root) return [];
+        curRow.push(root);
+        while (curRow.length > 0) {
+            let node = curRow.pop();
+            curLargest = (curLargest > node.val ? curLargest : node.val);
+            if (node.left) {
+                nextRow.push(node.left);
+            }
+            if (node.right) {
+                nextRow.push(node.right)
+            }
+            if (curRow.length === 0) {
+                result.push(curLargest);
+                curLargest = -Number.MAX_VALUE;
+                curRow = nextRow;
+                nextRow = [];
+            }
+        }
+        return result;
+    },
+}
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 let BTP = require('./binaryTreePath');
 let AMS = require('./arithmeticSlice');
+let LVTR = require('./largestValueInTreeRow');
 
 function testBinaryTreePath() {
     let root = BTP.createNode(3);
@@ -15,5 +16,17 @@ function testArithmeticSlice() {
     console.log(AMS.numberOfArithmeticSlices([1,2,3,4,7,8,9]));
 }
 
+function testLargestValueInTreeRow() {
+    let root = BTP.createNode(3);
+    root.left = BTP.createNode(1);
+    root.right = BTP.createNode(5);
+    root.left.left = BTP.createNode(5);
+    root.left.right = BTP.createNode(3);
+    root.right.right = BTP.createNode(9);
+    console.log('testing largest value in Tree row: ');
+    console.log(LVTR.largestValues(root));
+}
+
 // testBinaryTreePath();
-testArithmeticSlice();
+// testArithmeticSlice();
+testLargestValueInTreeRow();
